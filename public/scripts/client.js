41ac8da6285a135167fffc9d117bf9383219349a
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-12 21:40:46 +0000

    Implement AJAX calls for tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,19 +3,84 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
+const createTweetElement = function(tweetData) {
+  const avatar = tweetData.user.avatars;
+  const name = tweetData.user.name;
+  const handle = tweetData.user.handle;
+  const content = tweetData.content.text;
+  const date = tweetData.created_at;
+  const $tweet = `
+  <article class="tweet">
+    <header>
+      <img src="${avatar}" alt="Avatar for ${name}">
+      <p>${name}</p>
+      <p class="hashtag">${handle}</p>
+    </header>
+    <p>${content}</p>
+    <footer>
+      <p>${date}</p>
+      <span></span>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-heart"></i>
+      <i class="fa-solid fa-retweet"></i>
+    </footer>
+  </article>
+  `
+  return $tweet;
+};
+
+const renderData = function(tweets) {
+  for (const tweet of tweets) {
+    const $renderedTweet = createTweetElement(tweet);
+    $('.tweet-container').append($renderedTweet);
+  }
+};
+
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    dataType: 'JSON',
+    success: (fetchedTweets) => {
+      renderData(fetchedTweets);
+    }
+  })
+};
+
 $(document).ready(function() {
-  const $tweet = $('.tweet');
-  const $social = $('i');
-  $tweet.on('mouseenter', function() {
+  //- Hover event-watcher -//
+  $('.tweet').mouseenter(function() {
     $(this).addClass('withShadow');
   })
-  $tweet.on('mouseleave', function() {
+  $('.tweet').mouseleave(function() {
     $(this).removeClass('withShadow');
   })
-  $social.on('mouseenter', function() {
+  $('i').mouseenter(function() {
     $(this).addClass('hovering');
   })
-  $social.on('mouseleave', function() {
+  $('i').mouseleave(function() {
     $(this).removeClass('hovering');
   })
+  //- Form submit event-watcher -//
+  const $form = $('#submit-tweet');
+  $form.submit((event) => {
+    event.preventDefault();
+    //get the data
+    const dataToServer = $form.serialize();
+    console.log(dataToServer);
+    //Send the info to the server via a POST request
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: dataToServer,
+    })
+    .then((response) => {
+      console.log('response', response);
+    })
+    .catch((error) => {
+      console.log('error', error);
+    })
+  });
+  loadTweets();
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
+  const CHAR_MAX = 140;
   const $tweetText = $('#tweet-text');
   $tweetText.on('input', function(data) {
     const textLength = $(this).val().length;
     const $counter = $(this).siblings('.submit-counter').children('.counter')[0];
-    $counter.innerHTML = 140 - textLength;
+    $counter.innerHTML = CHAR_MAX - textLength;
     if ($counter.innerHTML < 0 && $($counter).attr('id') === 'counter') {
       $($counter).attr('id', 'error-text');
     } else if ($counter.innerHTML >= 0 && $($counter).attr('id') === 'error-text') {
