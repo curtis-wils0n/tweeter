31dd254e5bf87692f4d15f446a4922d0e7688626
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-13 06:06:09 +0000

    Fix bug where error-text id prevents counter from resuming.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   const CHAR_MAX = 140;
   const $tweetText = $('#tweet-text');
-  $tweetText.on('input', function(data) {
+  $tweetText.on('input', function() {
     const textLength = $(this).val().length;
-    const $counter = $(this).siblings('.submit-counter').children('#counter')[0];
+    const $counter = $(this).siblings('.submit-counter').children()[1];
     $counter.innerHTML = CHAR_MAX - textLength;
-    if ($counter.innerHTML < 0 && $($counter).attr('id') === 'counter') {
+    if ($counter.innerHTML < 0) {
       $($counter).attr('id', 'error-text');
     } else if ($counter.innerHTML >= 0 && $($counter).attr('id') === 'error-text') {
+      $($counter).removeAttr('id', 'error-text');
       $($counter).attr('id', 'counter');
     }
-  })
+  });
 });
